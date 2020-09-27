02097d86f5823c30ed9bdc58e29046795fac071d
boxior <1987boxior@gmail.com>
2020-09-27 19:04:55 +0300

    Updates

---
+++ precache-manifest.85d82e49f159274c2a8077d1212fa15f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ae30ab8e2504e0370fb07312863f72",
+    "url": "/test-task-github-api/index.html"
+  },
+  {
+    "revision": "0aaed351a47f2d1b67e7",
+    "url": "/test-task-github-api/static/css/4.cd846464.chunk.css"
+  },
+  {
+    "revision": "19027efb610787f04544",
+    "url": "/test-task-github-api/static/css/6.57eb4e20.chunk.css"
+  },
+  {
+    "revision": "6f1d48e76a3f84504599",
+    "url": "/test-task-github-api/static/css/7.f25c6616.chunk.css"
+  },
+  {
+    "revision": "576510aec890a0d10a8f",
+    "url": "/test-task-github-api/static/css/main.24ed6042.chunk.css"
+  },
+  {
+    "revision": "1be1e7d6363209e0100f",
+    "url": "/test-task-github-api/static/js/0.24f50f86.chunk.js"
+  },
+  {
+    "revision": "c18e1cefe3af534377cd",
+    "url": "/test-task-github-api/static/js/3.a6dda757.chunk.js"
+  },
+  {
+    "revision": "0aaed351a47f2d1b67e7",
+    "url": "/test-task-github-api/static/js/4.79482de6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-task-github-api/static/js/4.79482de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "870f8d2d8430f421e92c",
+    "url": "/test-task-github-api/static/js/5.cf3367ac.chunk.js"
+  },
+  {
+    "revision": "e5b3093c33a5f463c8b8116dbb3632d9",
+    "url": "/test-task-github-api/static/js/5.cf3367ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19027efb610787f04544",
+    "url": "/test-task-github-api/static/js/6.d0f8a4d1.chunk.js"
+  },
+  {
+    "revision": "6f1d48e76a3f84504599",
+    "url": "/test-task-github-api/static/js/7.51ca4480.chunk.js"
+  },
+  {
+    "revision": "576510aec890a0d10a8f",
+    "url": "/test-task-github-api/static/js/main.2b34559e.chunk.js"
+  },
+  {
+    "revision": "acb4be8e7a3a7727a967",
+    "url": "/test-task-github-api/static/js/runtime-main.629008cf.js"
+  }
+]);
